9379df411cb2d27a3d4e0f54f0e2b38c7e6fab02
ZsoltJuhasz <juhasz.zsolt98@gmail.com>
2021-12-02 12:13:43 +0100

    Kesz a bekeres es a felulet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'triangle';
+  base: string;
+  height: string;
+  area: string;
+  constructor() {
+    this.base = '';
+    this.height = '';
+    this.area = '';
+  }
+  calcArea(): any {
+    // console.log(this.base);
+    let area = Number(this.base) * Number(this.height) / 2;
+    this.base = '';
+    this.height = '';
+    this.area = String(area);
+  }
 }
